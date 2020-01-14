729a3483836ca5f181aa641a8cd5c40ca7e4c717
Lucas <lucasleeeeeeeee@gmail.com>
2020-01-14 17:27:18 +0800

    UpdateBooking: add form with basic UI elements

---
+++ UpdateBooking.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Session2
+{
+    partial class UpdateBooking
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.total_label = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.quantity_updown = new System.Windows.Forms.NumericUpDown();
+            this.update_button = new System.Windows.Forms.Button();
+            this.delete_button = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.quantity_updown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Red;
+            this.panel1.Location = new System.Drawing.Point(-2, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(803, 43);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Red;
+            this.panel2.Location = new System.Drawing.Point(-2, 409);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(803, 43);
+            this.panel2.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(-2, 84);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(803, 229);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(600, 316);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "TOTAL ($):";
+            // 
+            // total_label
+            // 
+            this.total_label.AutoSize = true;
+            this.total_label.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.total_label.Location = new System.Drawing.Point(703, 316);
+            this.total_label.Name = "total_label";
+            this.total_label.Size = new System.Drawing.Size(58, 18);
+            this.total_label.TabIndex = 5;
+            this.total_label.Text = "label3";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(378, 346);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(133, 18);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "New Quantity :";
+            // 
+            // quantity_updown
+            // 
+            this.quantity_updown.Location = new System.Drawing.Point(517, 346);
+            this.quantity_updown.Name = "quantity_updown";
+            this.quantity_updown.Size = new System.Drawing.Size(120, 20);
+            this.quantity_updown.TabIndex = 7;
+            // 
+            // update_button
+            // 
+            this.update_button.Location = new System.Drawing.Point(643, 346);
+            this.update_button.Name = "update_button";
+            this.update_button.Size = new System.Drawing.Size(120, 23);
+            this.update_button.TabIndex = 8;
+            this.update_button.Text = "Update Quantity";
+            this.update_button.UseVisualStyleBackColor = true;
+            // 
+            // delete_button
+            // 
+            this.delete_button.Location = new System.Drawing.Point(641, 375);
+            this.delete_button.Name = "delete_button";
+            this.delete_button.Size = new System.Drawing.Size(120, 23);
+            this.delete_button.TabIndex = 9;
+            this.delete_button.Text = "Delete";
+            this.delete_button.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(180, 44);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(438, 37);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Sponsor Manager Main Menu";
+            // 
+            // UpdateBooking
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.delete_button);
+            this.Controls.Add(this.update_button);
+            this.Controls.Add(this.quantity_updown);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.total_label);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "UpdateBooking";
+            this.Text = "UpdateBooking";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.quantity_updown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label total_label;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown quantity_updown;
+        private System.Windows.Forms.Button update_button;
+        private System.Windows.Forms.Button delete_button;
+        private System.Windows.Forms.Label label4;
+    }
+}
