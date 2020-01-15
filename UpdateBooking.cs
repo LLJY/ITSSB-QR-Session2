7a74516d5fd561ae6b466564987eb019ddf7050c
Lucas <lucasleeeeeeeee@gmail.com>
2020-01-15 15:31:13 +0800

    UpdateBooking: Define Async Method GetBookingUpdates
    gets information to fill up DataGridView

--- UpdateBooking.cs
+++ UpdateBooking.cs
@@ -14,7 +14,51 @@ namespace Session2
     {
         public UpdateBooking()
         {
+            Initialize();
+        }
+        public async void Initialize()
+        {
             InitializeComponent();
         }
+        /// <summary>
+        /// Async method to build the list of BookingUpdates
+        /// Count is from counting bookings for every id in packages.
+        /// </summary>
+        /// <returns></returns>
+        private async Task<List<BookingUpdate>> GetBookingUpdates()
+        {
+            var returnlist = new List<BookingUpdate>();
+            using (var db = new Session2Entities())
+            {
+                var packages = (from p in db.Packages
+                                where p.packageQuantity > 0
+                                select p).ToList();
+                foreach (var item in packages)
+                {
+                    var cnt = (from c in db.Bookings
+                               where c.packageIdFK == item.packageId
+                               select c).Count();
+                    var bu = new BookingUpdate()
+                    {
+                        ID = item.packageId,
+                        Tier = item.packageTier,
+                        Name = item.packageName,
+                        IndividualValue = (int)item.packageValue,
+                        QuantityBooked = cnt
+                    };
+                    returnlist.Add(bu);
+                }
+            }
+            return returnlist;
+        }
+    }
+    public class BookingUpdate
+    {
+        public int ID { get; set; }
+        public string Tier { get; set; }
+        public string Name { get; set; }
+        public int IndividualValue { get; set; }
+        public int QuantityBooked { get; set; }
+        public int SubTotalValue { get; set; }
     }
 }
